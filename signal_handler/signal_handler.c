92aeda456f2eba76d99f161d10ced95ad2e2dc11
Moritz Schilling <moritz.schilling@dfki.de>
2018-09-05 10:36:59 +0200

    Version to be tested for the ESROCOS test campaign
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

---
+++ signal_handler/signal_handler.c
@@ -0,0 +1,15 @@
+/* User code: This file will not be overwritten by TASTE. */
+
+#include "signal_handler.h"
+
+void signal_handler_startup()
+{
+    /* Write your initialization code here,
+       but do not make any call to a required interface. */
+}
+
+void signal_handler_PI_dummy_trigger()
+{
+    /* Write your code here! */
+}
+
