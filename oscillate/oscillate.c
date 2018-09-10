12093e26a2b1d4aaf37f1432d4786535dbb1e19e
Moritz Schilling <moritz.schilling@dfki.de>
2018-09-10 10:38:59 +0200

    Fixed oscillate function
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- oscillate/oscillate.c
+++ oscillate/oscillate.c
@@ -12,9 +12,9 @@
 
 /*
 * This is a test function to produce a motion pattern for the CAN device used in ESROCOS
-* Starting at 0 deg, this function will set the velocity to reach 45 deg.
-* If 45 deg have been reached the function will try to reach -45 deg.
-* Then it will indefintely cycle between 45 and -45 deg.
+* Starting at 0 deg, this function will set the velocity to reach 135 deg.
+* If 135 deg have been reached the function will try to reach 45 deg.
+* Then it will indefintely cycle between 45 and 135 deg.
 */
 
 enum State { REACH45P, REACH45N };
@@ -37,9 +37,10 @@ void oscillate_PI_status(const asn1SccBase_commands_Joints *IN_sample)
 {
     float position_rad = IN_sample->elements.arr[0].position;
     float position_deg = position_rad * 180.f / M_PI;
-    float position_ref = (state == REACH45N) ? 45.f : -45.f;
+    float position_ref = (state == REACH45P) ? 135.f : 45.f;
     float error = (position_ref - position_deg);
     float velocity_ref_deg = fabsf(error) > 1.f ? ((error > 0.f) ? 1.f : ((error < 0.f) ? -1.f : 0.f)) : error;
+    //printf("ref: %f cur: %f err: %f vel: %f\n", position_ref, position_deg, error, velocity_ref_deg);
 
     cmd.time.microseconds = getTimeInMicroseconds();
     cmd.names.nCount = 1;
