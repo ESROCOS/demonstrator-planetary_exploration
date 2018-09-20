58198c9f81b28b46e0201dedc88cb14a6f48e461
malte wirkus <malte.wirkus@dfki.de>
2018-09-20 14:57:43 +0100

    added code to signal handler

--- signal_handler/signal_handler.c
+++ signal_handler/signal_handler.c
@@ -1,12 +1,25 @@
 /* User code: This file will not be overwritten by TASTE. */
 
 #include "signal_handler.h"
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+void handle_signal( int signum )
+{
+	std::cout << "Interrupt signal (" << signum << ") received.\n"<<std::endl;
+	signal_handler_RI_shutdown();
+	sleep(1);
+	std::cout << "Terminating..."<<std::endl;
+	exit(0);
+}
 
 void signal_handler_startup()
 {
-    /* Write your initialization code here,
-       but do not make any call to a required interface. */
+    //Register signal handler for SIGINT to close log file
+    signal(SIGINT, handle_signal);
 }
+
 
 void signal_handler_PI_dummy_trigger()
 {
